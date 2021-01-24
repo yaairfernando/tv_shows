8ba0150384ce5891e6587e67e003b8f9e58306df
yairfacio <yair.facio11@gmail.com>
2021-01-23 18:56:11 -0600

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :tv_shows, only: [:show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tv_shows/:id/reviews', action: :index, controller: :reviews
 end
